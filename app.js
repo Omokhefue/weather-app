99b49708b9351162ff0695d5185e6e2bcf817259
Omokhefue <mokhefue@gmail.com>
2023-02-02 09:26:17 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const time = document.querySelector("img.time");
 const icon = document.querySelector(".icon img");
 const body = document.querySelector("body");
 const h1 = document.querySelector("h1");
+
+const forecast = new Forecast();
+
 const updateUI = (data) => {
   const { cityDets, weather } = data;
   details.innerHTML = `
 <h5 class="my-3">${cityDets.EnglishName}</h5>
 <div class="my-3">${weather.WeatherText}</div>
-<div class="my-1">${weather.LocalObservationDateTime
-.slice(11,16)}</div>
+<div class="my-1">${weather.LocalObservationDateTime.slice(11, 16)}</div>
 <div class="display-4 my-4">
   <span>${weather.Temperature.Metric.Value}</span>
   <span>&deg;C</span>
@@ -24,8 +26,8 @@ const updateUI = (data) => {
     body.style.backgroundColor = "rgb(2, 2, 20)";
     h1.classList.remove("text-muted");
     cityForm.classList.remove("text-muted");
-    h1.style.color='#fff'
-    cityForm.style.color='#fff'
+    h1.style.color = "#fff";
+    cityForm.style.color = "#fff";
   } else {
     body.style.backgroundColor = "#eeedec";
     h1.classList.add("text-muted");
@@ -37,21 +39,14 @@ const updateUI = (data) => {
   }
 };
 
-const updateCity = async (city) => {
-  const cityDets = await getCity(city);
-  const weather = await getWeather(cityDets.Key);
-  return {
-    cityDets,
-    weather,
-  };
-};
 
 cityForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const city = cityForm.city.value.trim();
   cityForm.reset();
   scrollTo(999999999999999, 999999999999);
-  updateCity(city)
+  forecast
+    .updateCity(city)
     .then((data) => {
       updateUI(data);
     })
@@ -61,9 +56,9 @@ cityForm.addEventListener("submit", (e) => {
 });
 
 if (localStorage.getItem("city")) {
-  updateCity(localStorage.getItem("city"))
-    .then((data) => {updateUI(data)
-  
+  forecast.updateCity(localStorage.getItem("city"))
+    .then((data) => {
+      updateUI(data);
     })
     .catch((err) => console.log(err));
 }
--- forecast.js
+++ forecast.js
@@ -1,20 +1,31 @@
-// require("dotenv").config();
-// let key = process.env.API_KEY;
+class Forecast {
+  constructor() {
+    this.key = key;
+    this.cityURI =
+      "http://dataservice.accuweather.com/locations/v1/cities/search";
+    this.weatherURI =
+      "http://dataservice.accuweather.com/currentconditions/v1/";
+  }
+  async updateCity(city) {
+    const cityDets = await this.getCity(city);
+    const weather = await this.getWeather(cityDets.Key);
+    return {
+      cityDets,
+      weather,
+    };
+  }
+  async getCity(city) {
+    const query = `?apikey=${this.key}&q=${city}`;
 
-const getCity = async (city) => {
-  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
-  const query = `?apikey=${key}&q=${city}`;
+    const response = await fetch(this.cityURI + query);
+    const data = await response.json();
+    return data[0];
+  }
+  async getWeather(id) {
+    const query = `${id}?apikey=${this.key}`;
 
-  const response = await fetch(base + query);
-  const data = await response.json();
-  return data[0];
-};
-
-const getWeather = async (id) => {
-  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
-  const query = `${id}?apikey=${key}`;
-
-  const response = await fetch(base + query);
-  const data = await response.json();
-  return data[0];
-};
+    const response = await fetch(this.weatherURI + query);
+    const data = await response.json();
+    return data[0];
+  }
+}
